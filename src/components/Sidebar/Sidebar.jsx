a8268d954e889571d3356cbffa6a2e20abd52001
quinbez <bezawitt9@gmail.com>
2023-02-26 14:14:47 -0800

    admin sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import Logo from '../../imgs/logo.png';
 import './Sidebar.css'
+import { SidebarData } from '../../data/data';
+import { UilSignOutAlt } from '@iconscout/react-unicons';
+import { useState } from 'react';
 
 const Sidebar = () => {
+
+  const [selected, setSelected] = useState(0)
+
   return (
-    <div>Sidebar</div>
-  )
+      <div className="Sidebar">
+        {/* logo */}
+        <div className="logo">
+          <img src={Logo} alt=''/>
+          <span>
+            <span>A</span>rt<span>S</span>ell
+          </span>
+        </div>
+        {/* menu */}
+        <div className="menu">
+             {SidebarData.map((item, index)=>{
+              return (
+                <div className={selected === index? 'menuItem active': 'menuItem'}
+                key = {index}
+                onClick = {()=>setSelected(index)}
+                >
+                  <item.icon/>
+                  <span>
+                    {item.heading}
+                  </span>
+                </div>
+              )
+             })}
+             <div className="menuItem">
+                <UilSignOutAlt />
+             </div>
+        </div>
+      </div>
+    )
 }
 
 export default Sidebar
---
+++ src/data/data.js
@@ -0,0 +1,31 @@
+// sidebar imports
+import {
+    UilEstate,
+    UilClipboard,
+    UilUsersAlt,
+    UilPackage,
+    UilChart,
+    UilSignOutAlt
+} from '@iconscout/react-unicons';
+
+// sidebar data
+export  const SidebarData = [
+    {    icon: UilEstate,
+        heading: "Dashboard", 
+    },
+    {    icon: UilUsersAlt,
+        heading: "User", 
+    },
+    {    icon: UilPackage,
+        heading: "Product", 
+    },
+    {    icon: UilChart,
+        heading: "Category", 
+    },
+    {    icon: UilClipboard,
+        heading: "Order", 
+    },
+    {    icon: UilClipboard,
+        heading: "Payment", 
+    },
+];
